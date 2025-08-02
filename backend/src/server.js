be56efda32071681d93a8448d8179464b40f4894
Supriya <supriyachandra2007@gmail.com>
2025-08-03 03:59:59 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from 'path';
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -14,16 +15,23 @@ dotenv.config();
 
 const app = express();
 const PORT= process.env.PORT || 5002;
+const __dirname = path.resolve(); // by cosole log __dirname it will give source for backend
 
 
 
 
 //middleware
-app.use(cors({
-    origin:"http://localhost:5173",
-}));  // before anyother as we are trying to send the response so first we have to set the cors for cors error   
+//corcs needed only in development as we have made it in production or made our website in 1 port address in node not on both reacrt and node
 
-app.use(express.json()); // thiis middlware will parse JSN bodies: i.e. get access to req.body
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+    origin:"http://localhost:5173",
+    }));       // before anyother as we are trying to send the response so first we have to set the cors for cors error   
+}
+
+
+
+app.use(express.json()); // this middlware will parse JSN bodies: i.e. get access to req.body
 app.use(rateLimiter);
 
 
@@ -65,6 +73,15 @@ app.use("/api/notes",notesRoutes);
 // app.use("/api/product",productRoutes);
 // app.use("/api/post",productRoutes);
 // app.use("/api/payment",productRoutes);
+
+// for one web path not on both 5173 & 5002 only on 5002
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist"))); // serve or optimise the react application
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html")) //display index.html
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,9 +2,12 @@ import axios from "axios";
 
 
 // axiosInstance or api
+//In production, ther is nothing called localhost, so we have to make this  dynamic
+const BASE_URL = import.meta.env.MODE ===   "development" ? "http://localhost:5002/api" : "/api"
 
 const api = axios.create({
-    baseURL:"http://localhost:5002/api",
+// baseURL: "http://localhost:5002/api" // for development
+    baseURL: BASE_URL, // Render will give us any name so we won't have localhost soo we'll make it dynamic 
 });
 
 export default api;
